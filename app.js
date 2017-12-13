8b354776fd66674fe8bd64a0bbc9bb105c8147b5
Emerson Stark <emersonwebdev@gmail.com>
2017-12-13 00:36:32 -0300

    Fixing some issues

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
 
-var transporter = nodemailer.createTransport({
-    service: 'Gmail',
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
     auth: {
-        user: 'email@gmail.com',
-        pass: 'password'
+        user: 'youruser@gmail.com',
+        pass: 'yourpassword'
     }
 });
 
-var mailOptions = {
-    from: 'Stark , <starkdev@gmail.com>', 
+const mailOptions = {
+    from: 'Stark , <stark@gmail.com>',
     //Lista de emails que você quer enviar (pode cadastrar vários, basta separar por " , ")
-    to: 'test@tes.com', 
+    to: 'thor@avengers.com',
     //Assunto do email
-    subject: 'Teste de email com NODEJS ', 
+    subject: 'Teste de email com Nodejs ',
     //Aqui você pode enviar toda a mensagem
     text: 'Enviando email com nodejs + nodemailer ',
     // podemos também utilizar um HTML
-    html: '<h1> MSGM</h1>' 
+    html: '<h4>Podemos enviar um html =)</h4>'
 };
 
 // Enviando email
-transporter.sendMail(mailOptions, function(error, info){
+transporter.sendMail(mailOptions, (error, info) => {
     if(error){
         return console.log(error);
     }
-    console.log('Message sent: ' + info.response);
-
+    console.log('Mensagem enviada: ' + info.response);
 });
